0f3ea69655721e0639822ed50659498c0888eab8
sadraouizeineb <sadraouizeineb@gmail.com>
2025-02-11 21:39:45 +0100

    fixes

--- TP1/Controllers/MovieController.cs
+++ TP1/Controllers/MovieController.cs
@@ -5,44 +5,35 @@ namespace TP1.Controllers
 {
     public class MovieController : Controller
     {
-        List<Movie> movies = new List<Movie>();
+        private static List<Movie> movies = new List<Movie>()
+        {
+            new Movie{id= 1, Name="Movie 1"},
+            new Movie{id= 2, Name="Movie 2"},
+            new Movie{id= 3, Name="Movie 3"},
+        };
+
         public IActionResult Index()
         {
-            List<Movie> movies = new List<Movie>()
-               {
-                new Movie{id= 1,Name="movie 1"},
-                new Movie{id=2,Name="movie 2"},
-                new Movie{id=3,Name="movie 3"},
-               };
             return View(movies);
         }
-        public IActionResult Edit1(int id)
-        {
-            return Content("Test Id" + id);
-        }
-        public IActionResult ByRelease(int Year, int month)
-        {
-            return Content("MovieYear" +    Year +   month);
-        }
 
-        public  IActionResult Edit(int id)
+        public IActionResult Edit(int id)
         {
-            
             var movie = movies.FirstOrDefault(m => m.id == id);
             if (movie == null)
                 return NotFound();
             return View(movie);
         }
+
         [HttpPost]
-        public ActionResult Edit(Movie movie)
+        public IActionResult Edit(Movie movie)
         {
-            
-            var existinMovie = movies.FirstOrDefault(m => m.id == movie.id);
-            if (existinMovie == null)
+            var existingMovie = movies.FirstOrDefault(m => m.id == movie.id);
+            if (existingMovie == null)
                 return NotFound();
-            existinMovie.Name = movie.Name;
+
+            existingMovie.Name = movie.Name;
             return RedirectToAction("Index");
         }
-
     }
 }
