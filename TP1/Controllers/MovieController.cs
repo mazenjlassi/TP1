aef79a33039efd982e7d802aaa0d6a278583dc62
jlassi mazen <jlmazen1073@gmail.com>
2025-02-11 20:59:26 +0100

    the error in the edit function

---
+++ TP1/Controllers/MovieController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TP1.Models;
+
+namespace TP1.Controllers
+{
+    public class MovieController : Controller
+    {
+        List<Movie> movies = new List<Movie>();
+        public IActionResult Index()
+        {
+            List<Movie> movies = new List<Movie>()
+               {
+                new Movie{id= 1,Name="movie 1"},
+                new Movie{id=2,Name="movie 2"},
+                new Movie{id=3,Name="movie 3"},
+               };
+            return View(movies);
+        }
+        public IActionResult Edit1(int id)
+        {
+            return Content("Test Id" + id);
+        }
+        public IActionResult ByRelease(int Year, int month)
+        {
+            return Content("MovieYear" +    Year +   month);
+        }
+
+        public  IActionResult Edit(int id)
+        {
+            
+            var movie = movies.FirstOrDefault(m => m.id == id);
+            if (movie == null)
+                return NotFound();
+            return View(movie);
+        }
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            
+            var existinMovie = movies.FirstOrDefault(m => m.id == movie.id);
+            if (existinMovie == null)
+                return NotFound();
+            existinMovie.Name = movie.Name;
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ TP1/Models/MovieCustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TP1.Models
+{
+    public class MovieCustomerViewModel
+    { 
+        public Movie Movie { get; set; }
+        public List <Customer>  customers { get; set; }
+    }
+}
